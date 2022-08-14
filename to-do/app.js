19f340dbb31c278848e8028468fa56861d55050c
Ben Wagner <bmwbenswagner@gmail.com>
2022-08-14 17:05:10 -0500

    Initialize document variables

---
+++ to-do/app.js
@@ -0,0 +1,3 @@
+const addTaskContainer = document.getElementById("add-task-container");
+const addTask = document.getElementById("add-task");
+const taskContainer = document.getElementById("task-container");
