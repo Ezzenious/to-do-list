3152c2206496bd6f1bb030f8b7894b524094a128
Ben Wagner <bmwbenswagner@gmail.com>
2022-08-14 20:51:23 -0500

    Set add button to create task element when clicked

--- to-do/app.js
+++ to-do/app.js
@@ -1,3 +1,13 @@
-const addTaskContainer = document.getElementById("add-task-container");
+const inputTask = document.getElementById("input-task");
 const addTask = document.getElementById("add-task");
 const taskContainer = document.getElementById("task-container");
+
+addTask.addEventListener("click", () => {
+  let task = document.createElement("div");
+  task.classList.add("task");
+
+  let li = document.createElement("li");
+  li.innerText = `${inputTask.value}`;
+  
+  task.appendChild(li);
+});
