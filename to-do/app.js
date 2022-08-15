112f9d073b000b7c926628b5cc12766ed1b87138
Ben Wagner <bmwbenswagner@gmail.com>
2022-08-14 20:59:03 -0500

    Add check and delete button to task element

--- to-do/app.js
+++ to-do/app.js
@@ -8,6 +8,16 @@ addTask.addEventListener("click", () => {
 
   let li = document.createElement("li");
   li.innerText = `${inputTask.value}`;
-  
+
   task.appendChild(li);
+
+  let checkBtn = document.createElement("button");
+  checkBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
+  checkBtn.classList.add("check-btn");
+  task.appendChild(checkBtn);
+
+  let deleteBtn = document.createElement("button");
+  deleteBtn.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+  deleteBtn.classList.add("delete-btn");
+  task.appendChild(deleteBtn);
 });
